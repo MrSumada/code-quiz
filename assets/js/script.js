314129bb5c726511c148ec6ad961afb9930f35ab
MrSumada <ricksamericana.com>
2022-01-10 00:30:09 -0800

    fixed event listener for question choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,8 @@ var startContainer = document.querySelector("#start-div");
 var timeLeft = document.querySelector("span");
 var time = 30;
 var questionContainerEl = document.querySelector("#question-container");
-var choiceEl = document.querySelector(".choice");
-// var correctAnswerEl = document.querySelector(".correct");
-// var wrongAnswerEl = document.querySelector(".wrong");
-
 var questionNumber = 0;
-
+var currentScore = 0
 var questions = [
     {
         question: "What does DOM stand for?",
@@ -35,7 +31,39 @@ var questions = [
         correct: 1, 
     },
     {
-        question: "What does DRAMA stand for?",
+        question: "What does ADRIAN stand for?",
+        choice1: "Diffuse Object Model",
+        choice2: "Document Object Model",
+        choice3: "Diffuse Objective Model",
+        choice4: "Domestic Objective Model",
+        correct: 4, 
+    },
+    {
+        question: "What does DOM stand for?",
+        choice1: "Diffuse Object Model",
+        choice2: "Document Object Model",
+        choice3: "Diffuse Objective Model",
+        choice4: "Domestic Objective Model",
+        correct: 2, 
+    },
+    {
+        question: "What does MOM stand for?",
+        choice1: "Diffuse Object Model",
+        choice2: "Document Object Model",
+        choice3: "Diffuse Objective Model",
+        choice4: "Domestic Objective Model",
+        correct: 3, 
+    },
+    {
+        question: "What does LOM stand for?",
+        choice1: "Diffuse Object Model",
+        choice2: "Document Object Model",
+        choice3: "Diffuse Objective Model",
+        choice4: "Domestic Objective Model",
+        correct: 1, 
+    },
+    {
+        question: "What does LAST stand for?",
         choice1: "Diffuse Object Model",
         choice2: "Document Object Model",
         choice3: "Diffuse Objective Model",
@@ -44,14 +72,28 @@ var questions = [
     }
 ];
 
+
+// START QUIZ FUNCTION
+
 function startQuiz() {
 
     startContainer.remove();
 
-    //ADD call for countdown Function??
+    countdown();
+
+    createQuestion(questionNumber);
+
+};
+
+
+// COUNTDOWN TIMER FUNCTION
+
+var countdown = function() {
+
     timeLeft.textContent = time;
 
     var timeCountdown = setInterval(function() {
+
         if (time < 2) {
             timeLeft.textContent = "";
             clearInterval(timeCountdown);
@@ -60,117 +102,117 @@ function startQuiz() {
         } else {
         time--;
         timeLeft.textContent = time;
+        };
+
+        if (questionNumber === questions.length) {
+            clearInterval(timeCountdown);
         }
     },1000);
 
-        createQuestion(questionNumber);
-    
 };
 
+
+// CREATE QUESTION FROM questions ARRAY FUNCTION
 
 var createQuestion = function(questionNumberArg) {
+    
+    //Create New Div
+    var questionDivEl = document.createElement("div");
+    questionDivEl.className = "question";
+    questionContainerEl.appendChild(questionDivEl);
 
-        var questionDivEl = document.createElement("div");
-        var questionHeadingEl = document.createElement("h1");
-        var choicesDivEl = document.createElement("div");
-        var questionChoiceEl = document.createElement("button");
+    //Create New QUESTION
+    var questionHeadingEl = document.createElement("h1");
+    questionHeadingEl.textContent = questions[questionNumberArg].question;
+    questionDivEl.appendChild(questionHeadingEl);
 
-        questionDivEl.className = "question";
-        questionContainerEl.appendChild(questionDivEl);
+    //Create Button Div
+    var choicesDivEl = document.createElement("div");
+    choicesDivEl.className = "choices"
+    questionDivEl.appendChild(choicesDivEl);
 
-        questionDivEl.className = "question";
-        questionContainerEl.appendChild(questionDivEl);
+    //Create FOUR buttons, IF/ELSE to add Correct or Wrong IDs
+    var questionChoiceEl = document.createElement("button");
+    questionChoiceEl.className = "choice choice1";
+    questionChoiceEl.textContent = questions[questionNumberArg].choice1;
+    if (questions[questionNumberArg].correct === 1) {
+        questionChoiceEl.setAttribute ("id", "correct");
+    } else {
+        questionChoiceEl.className += " wrong";
+    };
+    choicesDivEl.appendChild(questionChoiceEl);
 
-        questionHeadingEl.textContent = questions[questionNumberArg].question;
-        questionDivEl.appendChild(questionHeadingEl);
+    questionChoiceEl = document.createElement("button");
+    questionChoiceEl.className = "choice choice2";
+    questionChoiceEl.textContent = questions[questionNumberArg].choice2;
+    if (questions[questionNumberArg].correct === 2) {
+        questionChoiceEl.setAttribute ("id", "correct");
+    } else {
+        questionChoiceEl.className += " wrong";
+    };
+    choicesDivEl.appendChild(questionChoiceEl);
 
-        choicesDivEl.className = "choices"
-        questionDivEl.appendChild(choicesDivEl);
+    questionChoiceEl = document.createElement("button");
+    questionChoiceEl.className = "choice choice3";
+    questionChoiceEl.textContent = questions[questionNumberArg].choice3;
+    if (questions[questionNumberArg].correct === 3) {
+        questionChoiceEl.setAttribute ("id", "correct");
+    } else {
+        questionChoiceEl.className += " wrong";
+    };
+    choicesDivEl.appendChild(questionChoiceEl);
+    
+    questionChoiceEl = document.createElement("button");
+    questionChoiceEl.className = "choice choice4";
+    questionChoiceEl.textContent = questions[questionNumberArg].choice4;
+    if (questions[questionNumberArg].correct === 4) {
+        questionChoiceEl.setAttribute ("id", "correct");
+    } else {
+        questionChoiceEl.className += " wrong";
+    };
+    choicesDivEl.appendChild(questionChoiceEl);
 
-        questionChoiceEl.className = "choice choice1";
-        questionChoiceEl.textContent = questions[questionNumberArg].choice1;
-        if (questions[questionNumberArg].correct === 1) {
-            questionChoiceEl.setAttribute ("id", "#correct");
-        } else {
-            questionChoiceEl.setAttribute ("id", "#wrong");
+    //Create "Answer" Confirmation
+    var answerEl = document.createElement("h3");
+    answerEl.className = "answer";
+
+    //EITHER CHOICE event listener
+    var choiceEl = document.querySelector(".choice");
+    choicesDivEl.addEventListener("click", function(event){
+
+        //CORRECT or WRONG target match
+        var targetEl = event.target;
+        if (targetEl.matches("#correct")){
+            currentScore++;
+            answerEl.textContent = "Correct!";
+            choicesDivEl.appendChild(answerEl);
+        } 
+        else if (targetEl.matches(".wrong")){
+            answerEl.textContent = "Sorry! That's not right!";
+            choicesDivEl.appendChild(answerEl);
+            if (time >= 3) {
+                time = time - 3;
+            } else {
+                timeLeft = "";
+                score();
+            };
+            timeLeft.textContent = time;
         };
-        choicesDivEl.appendChild(questionChoiceEl);
 
-        questionChoiceEl = document.createElement("button");
-        questionChoiceEl.className = "choice choice2";
-        questionChoiceEl.textContent = questions[questionNumberArg].choice2;
-        if (questions[questionNumberArg].correct === 2) {
-            questionChoiceEl.setAttribute ("id", "#correct");
-        } else {
-            questionChoiceEl.setAttribute ("id", "#wrong");
-        };
-        choicesDivEl.appendChild(questionChoiceEl);
-
-        questionChoiceEl = document.createElement("button");
-        questionChoiceEl.className = "choice choice3";
-        questionChoiceEl.textContent = questions[questionNumberArg].choice3;
-        if (questions[questionNumberArg].correct === 3) {
-            questionChoiceEl.setAttribute ("id", "#correct");
-        } else {
-            questionChoiceEl.setAttribute ("id", "#wrong");
-        };
-        choicesDivEl.appendChild(questionChoiceEl);
-        
-        questionChoiceEl = document.createElement("button");
-        questionChoiceEl.className = "choice choice4";
-        questionChoiceEl.textContent = questions[questionNumberArg].choice4;
-        if (questions[questionNumberArg].correct === 4) {
-            questionChoiceEl.setAttribute ("id", "#correct");
-        } else {
-            questionChoiceEl.setAttribute ("id", "#wrong");
-        };
-        choicesDivEl.appendChild(questionChoiceEl);
-  
-        var answerCorrectness = function(answer) {
-
-        };
-
-        choicesDivEl.addEventListener("click", function(){
-     
-        questionNumber++;
-        console.log(questionNumber);
-        questionDivEl.remove();
-        if (questionNumber <= 3){
-            createQuestion(questionNumber);
-        } else {
-            console.log("WOO!")
-            score();
-        };             
-    });
+        //ONE SECOND DELAY to see ANSWER. THEN NEXT QUESTION or score()
+        var nextQuestion = setInterval(function() {
+            questionNumber++;
+            questionDivEl.remove();
+            if (questionNumber <= questions.length-1){
+                createQuestion(questionNumber);
+            } else {
+                clearInterval(nextQuestion);
+                console.log("Let's see the score!")
+                score();
+            };
+            clearInterval(nextQuestion);
+        },1000);
+    }, {once : true});
 }
 
-var choicesButtonHandler = function (event) {
-    
-    var targetEl = event.target;
-  
-    if (targetEl.matches("#correct")) {
-      console.log("CORRECT!");
-    } else if (targetEl.matches("#wrong")) {
-      console.log("wrong.");
-    }
-  };
-
-function checkAnswer() {
-
-    correctAnswerEl.addEventListener("click", function() {
-        console.log("Good!");
-
-    });
-
-    wrongAnswerEl.addEventListener("click", function() {
-        console.log("Bad!");
-        time = time - 2;
-        timeLeft.textContent = time;
-    });
-
-
-};
-
 startButtonEl.addEventListener("click", startQuiz);
-
-choiceEl.addEventListener("click", choicesButtonHandler);
